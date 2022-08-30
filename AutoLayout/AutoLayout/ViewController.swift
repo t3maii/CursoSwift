8253755e614f9e77976dfefc45436f60a5ca06f5
LordPinguim <jf.lipeto@gmail.com>
2022-08-30 18:12:18 -0400

    Layout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by ICMMAC09-BF7F on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
